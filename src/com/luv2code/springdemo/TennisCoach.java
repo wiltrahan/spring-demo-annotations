57e009b6dd13fd21be14ddeba5b21baa23c24329
wiltrahan <twilorip@gmail.com>
2018-04-14 19:51:55 -0400

    qualifier dependency injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	
 	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 	//define a default constructor
@@ -31,7 +33,7 @@ public class TennisCoach implements Coach {
 //		fortuneService = theFortuneService;
 //	}
 	@Autowired
-	public void myTest(FortuneService theFortuneService) {
+	public void myTest() {
 		System.out.println("MY TEST");
 	}
 
